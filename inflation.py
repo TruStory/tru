0d15d503900448f616bc2375ce3ec88532928603
Shane Vitarana <shanev@gmail.com>
2019-08-24 19:51:41 -0700

    variable interest

--- inflation.py
+++ inflation.py
@@ -1,7 +1,7 @@
 print("Compute tru inflation over time...\n")
 
 def print_pools(userPool, validatorPool, communityPool, stakeholderPool):
-  print("user pool " + f'{userPool:,.2f}' + " validator pool " + f'{validatorPool:,.2f}' + " community pool " + f'{communityPool:,.2f}' + " stake pool " + f'{stakeholderPool:,.2f}')
+  print("user pool " + f'{userPool:,.2f}' + " validator pool " + f'{validatorPool:,.2f}' + " community pool " + f'{communityPool:,.2f}' + " stakeholder pool " + f'{stakeholderPool:,.2f}')
 
 # current numbers
 # total supply = user balances + pending stake
@@ -17,7 +17,7 @@ stakeholderPool = 1000000.00  # 1,000,000tru
 
 totalSupply = userPool + validatorPool + communityPool + stakeholderPool
 print_pools(userPool, validatorPool, communityPool, stakeholderPool)
-print("total initial supply: "+ f'{totalSupply:,.2f}' + "\n")
+print("total initial supply: "+ f'{totalSupply:,.2f}')
 
 userAlloc = 0.4
 validatorAlloc = 0.2
@@ -28,19 +28,34 @@ stakeholderAlloc = 0.2
 # b/c need leave adequate amount of token liquid for trade
 # cosmos inflation changes between 20% and 70% (high staking to low staking)
 # inflation goes from 20% (when nearly 66% staked) to 70% (when closer to 0% staked)
-inflation = 0.6 # because we have low staking (10%)
+inflationRate = 0.6 # because we have low staking (10%)
+print("inflation           : %.2f percent" % (inflationRate * 100))
 
 # play with these values
-userInterest = 1.0 #105%
-valInterest = 1.0  #105%
+# fixed interest
+# userInterest = 1.05 #105%
+# valInterest = 1.05  #105%
+# variable interest based on inflation
+userInterest = inflationRate + (inflationRate * 0.75)
+valInterest = inflationRate + (inflationRate * 0.75)
+print("user interest       : %.2f percent" % (userInterest * 100))
+print("validator interest  : %.2f percent" % (valInterest * 100))
 
+initialGift = 300.0
+usersPerMonth = 250
+userOwned = usersPerMonth * 12 * initialGift
 totalUserOwned = 0.0
-totalValidatorOwned = 1000000.0
+
+totalValidatorStaked = validatorPool
+validatorPool = validatorPool - totalValidatorStaked
+totalSupply = userPool + validatorPool + communityPool + stakeholderPool
+print_pools(userPool, validatorPool, communityPool, stakeholderPool)
+print("")
 
 for year in range(0,10):
-  annualProvision = inflation * totalSupply
+  annualProvision = inflationRate * totalSupply
   print("year %d" % (year +1))
-  print("annual provision: " + f'{annualProvision:,.2f}')
+  print("annual inflation: " + f'{annualProvision:,.2f}')
 
   # increase pools
   userPool = userPool + (annualProvision * userAlloc)
@@ -49,28 +64,32 @@ for year in range(0,10):
   stakeholderPool = stakeholderPool + (annualProvision * stakeholderAlloc)
   
   # account for new user gifts
-  userOwned = 250 * 12 * 300 # 250 users a month
   userPool = userPool - userOwned
   totalUserOwned = totalUserOwned + userOwned
 
+  # validatorOwned = 
+
   # account for staking...
-  # if 66% user funds are being staked
-  userStaked = totalUserOwned * 0.66
+  # if 66% of new user funds are staked
+  userStaked = userOwned * 0.66
   userStakedInterest = userStaked * userInterest
+  userOwned = userOwned + userStakedInterest
+  totalUserOwned = totalUserOwned + userOwned
   userPool = userPool - userStakedInterest
+  # userOwned = userOwned + userStakedInterest
 
   # if 66% validator funds are being staked
   # valStaked = totalValidatorOwned * 0.66
   # valStakedInterest = valStaked * valInterest
   # validatorPool = validatorPool - valStakedInterest
 
+  validatorInterest = totalValidatorStaked * valInterest
+
   print_pools(userPool, validatorPool, communityPool, stakeholderPool)
 
-  # totalSupply = userPool + userOwned + userStakedInterest + validatorPool + valStakedInterest + communityPool + stakeholderPool
-  totalSupply = userPool + userOwned + userStakedInterest + validatorPool + communityPool + stakeholderPool
-
-  # total supply with no staking...
   # totalSupply = userPool + userOwned + validatorPool + communityPool + stakeholderPool
+  totalSupply = userPool + userOwned + validatorPool + validatorInterest + communityPool + stakeholderPool
+
   print("total user owned:      " + f'{totalUserOwned:,.2f}')
-  print("total validator owned: " + f'{totalValidatorOwned:,.2f}')
+  # print("total validator owned: " + f'{totalValidatorOwned:,.2f}')
   print("")
